1ea2366f3abb3771cb584eb372e2e881f92973f5
root <root@ubuntu-1310>
2014-07-22 16:33:30 +0000

    Add fallback for custom config of salt, length and alphabets.

--- src/Ludo237/Hashids/HashidsServiceProvider.php
+++ src/Ludo237/Hashids/HashidsServiceProvider.php
@@ -28,13 +28,79 @@ class HashidsServiceProvider extends ServiceProvider
 
 	protected function registerHashids()
 	{
-		$this->app->bind('Hashids\Hashids', function ($app) {
-			return new Hashids(
-				$app['config']['app.key'],
-				$app['config']['hashids::length'],
-				$app['config']['hashids::alphabet']
+		$self = $this;
+
+		$this->app->bind('Hashids\Hashids', function ($app) use ($self) {
+
+			$params = $self->getParams(); 
+
+			return new Hashids(			
+				$params['salt'],
+				$params['length'],
+				$params['alphabet']
 			);
 		});
+	}
+
+	/**
+	 * Get the salt, length and alphabet params, used for encrypting and decrypting hashes.
+	 *
+	 * @return string
+	 */
+	public function getParams()
+	{
+		return array(
+			'salt' => $this->getSalt(), 
+			'length' => $this->getLength(), 
+			'alphabet' => $this->getAlphabet()
+			);
+	}
+
+	/**
+	 * Get the length used for encrypting and decrypting hashes.
+	 *
+	 * @return string
+	 */
+	public function getSalt()
+	{
+		$salt = $this->app['config']['hashids::salt'];
+
+		if ( ! $salt) {
+			$salt = $this->app['config']['app.key'];
+		}
+
+		return $salt;
+	}
+
+	/**
+	 * Get the length used for the length of the hash.
+	 *
+	 * @return string
+	 */
+	public function getLength()
+	{
+		$length = $this->app['config']['hashids::length'];
+
+		if (! $length) {
+			$length = 7;
+		}
+
+		return $length;
+	}
+
+	/**
+	 * Get the alphabet used as base for encrypting and decrypting hashes.
+	 *
+	 * @return string
+	 */
+	public function getAlphabet()
+	{
+		$alphabet = $this->app['config']['hashids::alphabet'];
+
+		if (! $alphabet) {
+			$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+		}		
+		return $alphabet;
 	}
 
 	/**
